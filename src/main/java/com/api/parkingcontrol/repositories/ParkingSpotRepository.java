909cfc732adbcf5709fede55da0400e150055ccb
Danilo França <danilo.franca01@gmail.com>
2022-02-09 01:35:30 -0300

    Initial commit

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,20 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+    List<ParkingSpotModel> findAll();
+    @Query(value = "SELECT * FROM tb_parking_spot WHERE registration_date BETWEEN ?1 AND ?2",nativeQuery = true)
+    List<ParkingSpotModel> findByDate(LocalDateTime initialDate, LocalDateTime finalDate);
+}
