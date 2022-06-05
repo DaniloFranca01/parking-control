8d79019b39f923862b8cc5600afc8de3966f23f6
Danilo França <danilo.franca01@gmail.com>
2022-06-05 16:12:31 +0000

    Configurando o spring security.

--- src/main/java/com/api/parkingcontrol/models/AppUser.java
+++ src/main/java/com/api/parkingcontrol/models/AppUser.java
@@ -15,16 +15,18 @@ public class AppUser {
     private UUID id;
     private String name;
     private String username;
+    private String password;
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<AppRole> roles = new ArrayList<>();
 
     public AppUser() {
     }
 
-    public AppUser(UUID id, String name, String username, Collection<AppRole> roles) {
+    public AppUser(UUID id, String name, String username, String password, Collection<AppRole> roles) {
         this.id = id;
         this.name = name;
         this.username = username;
+        this.password = password;
         this.roles = roles;
     }
 
@@ -50,6 +52,14 @@ public class AppUser {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Collection<AppRole> getRoles() {
--- src/main/java/com/api/parkingcontrol/services/AppUserServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/services/AppUserServiceImpl.java
@@ -4,14 +4,21 @@ import com.api.parkingcontrol.models.AppRole;
 import com.api.parkingcontrol.models.AppUser;
 import com.api.parkingcontrol.repositories.AppRoleRepository;
 import com.api.parkingcontrol.repositories.AppUserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @Transactional
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
     private final AppUserRepository appUserRepository;
     private final AppRoleRepository appRoleRepository;
 
@@ -19,7 +26,18 @@ public class AppUserServiceImpl implements AppUserService {
         this.appUserRepository = appUserRepository;
         this.appRoleRepository = appRoleRepository;
     }
-
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = appUserRepository.findByUserName(username);
+        if (user == null){
+           throw new UsernameNotFoundException("User not found in the database");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(appRole -> {
+            authorities.add(new SimpleGrantedAuthority(appRole.getName()));
+        });
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
     @Override
     public AppUser saveUser(AppUser appUser) {
         return appUserRepository.save(appUser);
@@ -46,4 +64,5 @@ public class AppUserServiceImpl implements AppUserService {
     public List<AppUser> getUsers() {
         return appUserRepository.findAll();
     }
+
 }
